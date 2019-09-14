87a7225a7d3e13ae85b8cc121a3a09d84b69cbea
Luis Fernando Valera Bernal <lvalerab@gmail.com>
2019-09-14 17:09:08 +0200

    Pagina estatica con elementos externos

--- app.js
+++ app.js
@@ -2,7 +2,24 @@ const express= require('express');
 
 const app =express();
 
-app.get('/', (requets, response) => {
+/*Middleware*/
+var staticOptions={
+    dotfiles:'ignore',
+    etag:false,
+    extensions:['html','htm','js'],
+    index:true,
+    maxAge:'1d',
+    redirect:false,
+    setHeaders:function(res,path,stat) {
+        res.set('x-timestamp',Date.now());
+        res.set('x-powered-by','LFVB Node Server 1.0.0');
+    }
+};
+
+app.use(express.static('./public')); //Este middleware sirve el contenido estatico de public
+
+/*
+app.get('/primera', (requets, response) => {
     response.send('Hola mundo');
 });
 
@@ -12,7 +29,9 @@ app.get('/data', (request,response) => {
     }
     response.send(salida);
 });
+*/
 
 app.listen(8080,()=> {
     console.log('Escuchando peticiones en el puerto 8080');
+    console.log(`${__dirname}`)
 });
